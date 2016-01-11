e956194c78a8f37259bc85ac73c0c6715d5f1b53
ERIKAWHO <Erika@RefreshMediaResources.com>
2016-01-11 12:13:06 -0800

    Update postData

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,4 +1,4 @@
-function start(response) {
+function start(response, postData) {
   console.log("Request handler 'start' was called.");
 
 var body = '<html>'+
@@ -13,15 +13,16 @@ var body = '<html>'+
   '</form>'+
   '</body>'+
   '</html>';
+
   response.writeHead(200, {"Content-Type": "text/html"});
   response.write(body);
   response.end();
 }
 
-function upload(response) {
+function upload(response, postData) {
   console.log("Request handler 'upload' was called.");
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("Hello Upload");
+  response.write("You've sent: " + postData);
   response.end();
 }
 
